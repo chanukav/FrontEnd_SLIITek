f98c3a5e0cb197d330a17a9b7b3f55b1818a1ff6
Chanuka Vithanage <144988968+chanukav@users.noreply.github.com>
2026-03-22 23:19:15 +0530

    feat: Implement user dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,12 @@ import { Users } from "./pages/admin/Users/Users"
 import { Reports } from "./pages/admin/Reports/Reports"
 import { Notifications } from "./pages/admin/Notifications/Notifications"
 import { Settings } from "./pages/admin/Settings/Settings"
+import { UserLayout } from "./components/user/layout/UserLayout"
+import { Dashboard as UserDashboard } from "./pages/user/Dashboard/Dashboard"
+import { Profile as UserProfile } from "./pages/user/Profile/Profile"
+import { Notifications as UserNotifications } from "./pages/user/Notifications/Notifications"
+import { Messages as UserMessages } from "./pages/user/Messages/Messages"
+import { Settings as UserSettings } from "./pages/user/Settings/Settings"
 
 function App() {
   return (
@@ -16,6 +22,13 @@ function App() {
           <Route path="reports" element={<Reports />} />
           <Route path="notifications" element={<Notifications />} />
           <Route path="settings" element={<Settings />} />
+        </Route>
+        <Route path="/user" element={<UserLayout />}>
+          <Route index element={<UserDashboard />} />
+          <Route path="profile" element={<UserProfile />} />
+          <Route path="notifications" element={<UserNotifications />} />
+          <Route path="messages" element={<UserMessages />} />
+          <Route path="settings" element={<UserSettings />} />
         </Route>
       </Routes>
     </Router>
