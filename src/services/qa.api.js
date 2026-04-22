5af10d646c7b6fe7d80049e9eafbdbd79db9dfeb
HirunK <147022713+HirunK@users.noreply.github.com>
2026-04-22 02:23:23 -0700

    adding validation

--- src/services/qa.api.js
+++ src/services/qa.api.js
@@ -74,7 +74,7 @@ export const qaApi = {
   deleteQuestion: async (id) => unwrap(await api.delete(`/questions/${id}`, withAuth())),
 
   getAnswersByQuestion: async (questionId) =>
-    unwrap(await api.get(`/answers/question/${questionId}`, withAuth())),
+    unwrap(await api.get(`/answers/question/${questionId}`)),
   postAnswer: async (questionId, payload) =>
     unwrap(await api.post(`/answers/${questionId}`, payload, withAuth())),
   editAnswer: async (answerId, payload) =>
@@ -87,6 +87,20 @@ export const qaApi = {
     unwrap(await api.post(`/answers/${answerId}/vote`, payload, withAuth())),
   unvoteAnswer: async (answerId) =>
     unwrap(await api.delete(`/answers/${answerId}/vote`, withAuth())),
+  uploadAnswerImages: async (answerId, files) => {
+    const form = new FormData();
+    for (const f of files) {
+      form.append("images", f);
+    }
+    return unwrap(await api.post(`/answers/${answerId}/images`, form, withAuth()));
+  },
+  removeAnswerImage: async (answerId, url) =>
+    unwrap(
+      await api.delete(`/answers/${answerId}/images`, {
+        ...withAuth(),
+        data: { url },
+      })
+    ),
   addCommentToAnswer: async (answerId, payload) =>
     unwrap(await api.post(`/answers/${answerId}/comments`, payload, withAuth())),
 };
